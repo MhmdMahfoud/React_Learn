09747850ed0e500eee0207e36b4ba110e578c0bf
Mhmd Mahfoud <engmohammedmahfoud2001@gmail.com>
2025-07-02 17:13:29 +0300

    Acess The Event

--- my-react-app/src/Button.jsx
+++ my-react-app/src/Button.jsx
@@ -1,19 +1,10 @@
 import React from 'react'
+import pic from './assets/PF.webP'
 
 function Button() {
-  let count=0;
-    const handelClick = ()=>
-    {
-     count++;
-     if(count >3)
-        console.log("stop Playing")
-     else{
-        console.log("you have count" , count )
-     }
-    }
- 
-  return(
-     <button onClick={handelClick} >Count </button>
+    const handelCLick = (e)=> e.target.style.display="none"
+  return (
+   <button onClick={(e)=>handelCLick(e)}> <img src={pic}></img></button>
   )
 }
 
